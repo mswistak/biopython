53ee2b44f30b764584641dd187a97593fda23569
Marcin Świstak <m.swistak@students.mimuw.edu.pl>
2014-11-23 15:06:20 +0100

    Method stubs for ZPObject class.

---
+++ Bio/ZP/ZPObject.py
@@ -0,0 +1,46 @@
+
+
+
+class ZPObject():
+
+	def __init__(self, filename=None, regionList=None, segmentSize=1000, changeAllowance=0.1):
+		
+
+	def getSegmentSize(self):
+
+
+	def setSegmentSize(self, size):
+
+
+	def getRegion(self, regionName):
+
+
+	def getRegions(self):
+
+
+	def getRegionList(self):
+
+
+	def addRegion(self, region):
+
+
+	def delRegion(self, regionName):
+
+
+	def updateRegion(self, region, regionName):
+
+
+	def getSequence(self, regionName, start=0, end=None):
+
+
+	def setSequence(self, sequence, regionName, start=0, end=None):
+
+
+	def addSequence(self, sequence, region):
+
+
+	def addSeguences(self, seguences, regions):
+
+
+	def save(self, filename):
+
