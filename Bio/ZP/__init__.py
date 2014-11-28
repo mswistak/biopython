d307ef9e26785fcc806cc1af93363c487a4c24bd
Marcin Świstak <m.swistak@students.mimuw.edu.pl>
2014-11-28 23:17:49 +0100

    Loading objects

--- Bio/ZP/__init__.py
+++ Bio/ZP/__init__.py
@@ -1,6 +1,15 @@
+import tarfile, os
+import cPickle as pickle
 
 
 from ZPObject import ZPObject
 from Region import Region
 from Segment import Segment
 
+
+def load(archivePath):
+	with tarfile.open(archivePath, "r") as tar:
+		zp = pickle.load( tar.extractfile("head") )
+
+	zp._setPath(archivePath)
+	return zp
