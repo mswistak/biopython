c64f48a3b9cf0de3da8a9c05d99b2e03fb95c097
Marcin Świstak <m.swistak@students.mimuw.edu.pl>
2014-11-29 19:08:15 +0100

    Adding comments in __init__.py

--- Bio/ZP/__init__.py
+++ Bio/ZP/__init__.py
@@ -8,8 +8,13 @@ from Segment import Segment
 
 
 def load(archivePath):
+	"""Function for loading ZPObjects from archive files."""
+
+	#extract ZPObject from archive
 	with tarfile.open(archivePath, "r") as tar:
 		zp = pickle.load( tar.extractfile("head") )
 
+	#change archivePath for ZPObject in case it is other than saved in
+	#extracted instance
 	zp._setPath(archivePath)
 	return zp
